b2f3bcfbc8748505060919d12725e2aade75ebac
Erik Hoffer <erik.hoffer@student.htldornbirn.at>
2025-02-07 17:29:17 +0100

    commit JS exercise 7.1

---
+++ JS/class/index1.js
@@ -0,0 +1,90 @@
+// 1. keyword: let
+// 2. variable: result
+// 3. expression: 1 + 2
+// 4. zuweisung: =
+// 5. terminator: ;
+// ==> statement: let result = 1 + 2;
+let result = 1 + 2; //3
+
+// 6. funcion: console.log
+// 7. argument: result
+// 8. terminator: ;
+// ==> statement: console.log(rusult);
+console.log(result);
+
+// k = 3; d = 2;
+// f(x) = k * x + d
+// f(5) = 3 * 5 + 2 = 17
+
+// code blocks
+{
+    let a = 10;
+    let b = 20;
+
+    let result = a + b;
+}
+
+// calculations
+
+let a1 = 10;
+let b1 = 20;
+
+// addition
+let sum = a1 + b1;
+
+// subtraktion
+let difference = a1 - b1;
+
+// multiplication
+let product = a1 * b1;
+
+// division
+let quotient = a1 / b1;
+
+// modulo
+let remainder = a1 % b1;
+
+// exponentiation
+let power = a1 ** b1;
+
+// expression
+let expression = (a1 + b1) * b1;
+
+// assigment
+a1 = 23;
+b1 = 42;
+
+// if statement
+if (a1 < b1) {
+    console.log('a1 is smaller than b1');
+} else {
+    console.log('a1 is not smaller than b1');
+}
+
+// if - else if - else statement
+if (a1 == b1) {
+    console.log('a1 equals b1');
+} else if (a1 < b1) {
+    console.log('a1 is not smaller than b1');
+} else {
+    console.log('a1 is greater than b1');
+}
+
+// loops
+// for loop
+for (let i = 0; i < 5; i++) {
+    console.log('durchlauf: ' + i);
+    console.log(i);
+}
+
+
+let integer = 213;
+let floatingPoint = 3.141;
+let boolean = true;
+
+// strings (Zeichenketten)
+let string = 'Hello World';
+let string2 = 'Hallo';
+
+let string1 = string + string2;
+console.log('The result is: ' + result);
