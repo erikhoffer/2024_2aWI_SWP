2c968f953e4aa42df2d9b676d718e286575e9fea
Erik Hoffer <erik.hoffer@student.htldornbirn.at>
2025-03-28 16:30:34 +0100

    commit exercise 11.2

--- JS/exercises/Exercise 11/Exercise 2/index.js
+++ JS/exercises/Exercise 11/Exercise 2/index.js
@@ -1,12 +1,17 @@
 let binary = '10101100';
 
-let result = opposite(binary);
+let result = Invert(binary);
 
 console.log(result);
 
 function Invert(bin) {
     let inverted = '';
     for (let i = 0; i < bin.length; i++) {
-
+        if (bin[i] === '0') {
+            inverted += '1';
+        } else {
+            inverted += '0';
+        }
     }
+    return inverted;
 }
